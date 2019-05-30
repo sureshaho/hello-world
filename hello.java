09be7547816d960068859adade7239c712fd9d61
sureshaho <40929036+sureshaho@users.noreply.github.com>
2019-05-30 18:39:12 +0530

    small
    small changes

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
       public static void main(String[] args){ 
           for (int i=1;i<=15;i++){
-            System.out.println("hello world.."+i);
+            System.out.println("hello world.HOOOOOO."+i);
            }
        }
 }
