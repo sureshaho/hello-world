f97532ce2cdc913c075598d44593b66d5983b474
sureshaho <40929036+sureshaho@users.noreply.github.com>
2019-05-30 17:22:21 +0530

    chnaged coumt

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
       public static void main(String[] args){ 
-          for (int i=1;i<=10;i++){
+          for (int i=1;i<=15;i++){
             System.out.println("hello world.."+i);
            }
        }
